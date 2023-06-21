1f43a1d375455b58e9d74c8e03320805c33907e3
ekrem <ekremufacik07@gmail.com>
2023-06-21 17:43:07 +0300

    login guest

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,9 +8,16 @@ import { Link } from "react-router-dom";
 import LoginForm, { loginSchema } from "../components/LoginForm";
 
 import useAuthCalls from "../hooks/useAuthCalls";
+import { LoadingButton } from "@mui/lab";
+import { useSelector } from "react-redux";
 
 const Login = () => {
   const { login } = useAuthCalls();
+  const { loading } = useSelector((state) => state.auth);
+  const guest = {
+    email: process.env.REACT_APP_GUEST_EMAIL,
+    password: process.env.REACT_APP_GUEST_PASSWORD,
+  };
 
   return (
     <Container maxWidth="sm">
@@ -43,6 +50,19 @@ const Login = () => {
         }}
         component={(props) => <LoginForm {...props} />}
       ></Formik>
+      <LoadingButton
+        // loading={loading}
+        loadingPosition="center"
+        variant="outlined"
+        onClick={() => login(guest)}
+        sx={{
+          mt: 2,
+          mx: "auto",
+          display: "block",
+        }}
+      >
+        Login as Guest
+      </LoadingButton>
       <Box sx={{ textAlign: "center", mb: 4, mt: 2 }}>
         <Link to="/register">Do you have not an account?</Link>
       </Box>
